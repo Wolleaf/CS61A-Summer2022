d0892f26f16da31d80b38842a1b407625e7d895c
Wolleaf Lee <744901920@qq.com>
2022-09-03 16:40:34 +0800

    feat:hw03

---
+++ hw/hw03/hw03.py
@@ -0,0 +1,150 @@
+HW_SOURCE_FILE = __file__
+
+
+def num_eights(pos):
+    """Returns the number of times 8 appears as a digit of pos.
+
+    >>> num_eights(3)
+    0
+    >>> num_eights(8)
+    1
+    >>> num_eights(88888888)
+    8
+    >>> num_eights(2638)
+    1
+    >>> num_eights(86380)
+    2
+    >>> num_eights(12345)
+    0
+    >>> from construct_check import check
+    >>> # ban all assignment statements
+    >>> check(HW_SOURCE_FILE, 'num_eights',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    if pos == 0:
+        return 0
+    if pos % 10 == 8:
+        return num_eights(pos // 10) + 1
+    else:
+        return num_eights(pos // 10)
+
+
+def pingpong(n):
+    """Return the nth element of the ping-pong sequence.
+
+    >>> pingpong(8)
+    8
+    >>> pingpong(10)
+    6
+    >>> pingpong(15)
+    1
+    >>> pingpong(21)
+    -1
+    >>> pingpong(22)
+    -2
+    >>> pingpong(30)
+    -2
+    >>> pingpong(68)
+    0
+    >>> pingpong(69)
+    -1
+    >>> pingpong(80)
+    0
+    >>> pingpong(81)
+    1
+    >>> pingpong(82)
+    0
+    >>> pingpong(100)
+    -6
+    >>> from construct_check import check
+    >>> # ban assignment statements
+    >>> check(HW_SOURCE_FILE, 'pingpong',
+    ...       ['Assign', 'AnnAssign', 'AugAssign', 'NamedExpr'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def helper_func(num, index, step):
+        if index == n:
+            return num
+        if index % 8 == 0 or num_eights(index):
+            return helper_func(num - step, index + 1, -step)
+        else:
+            return helper_func(num + step, index + 1, step)
+    return helper_func(1, 1, 1)
+
+
+def get_larger_coin(coin):
+    """Returns the next larger coin in order.
+    >>> get_larger_coin(1)
+    5
+    >>> get_larger_coin(5)
+    10
+    >>> get_larger_coin(10)
+    25
+    >>> get_larger_coin(2) # Other values return None
+    """
+    if coin == 1:
+        return 5
+    elif coin == 5:
+        return 10
+    elif coin == 10:
+        return 25
+
+
+def get_smaller_coin(coin):
+    """Returns the next smaller coin in order.
+    >>> get_smaller_coin(25)
+    10
+    >>> get_smaller_coin(10)
+    5
+    >>> get_smaller_coin(5)
+    1
+    >>> get_smaller_coin(2) # Other values return None
+    """
+    if coin == 25:
+        return 10
+    elif coin == 10:
+        return 5
+    elif coin == 5:
+        return 1
+
+
+def count_coins(change):
+    """Return the number of ways to make change using coins of value of 1, 5, 10, 25.
+    >>> count_coins(15)
+    6
+    >>> count_coins(10)
+    4
+    >>> count_coins(20)
+    9
+    >>> count_coins(100) # How many ways to make change for a dollar?
+    242
+    >>> count_coins(200)
+    1463
+    >>> from construct_check import check
+    >>> # ban iteration
+    >>> check(HW_SOURCE_FILE, 'count_coins', ['While', 'For'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    def helper_func(money, face_value):
+        if money < 0:
+            return 0
+        if face_value == 1:
+            return 1
+        if face_value is None:
+            return 0
+        with_value = helper_func(money - face_value, face_value)
+        without_value = helper_func(money, get_smaller_coin(face_value))
+        return with_value + without_value
+    if change < 5:
+        max_value = 1
+    elif 5 <= change < 10:
+        max_value = 5
+    elif 10 <= change < 25:
+        max_value = 10
+    else:
+        max_value = 25
+    return helper_func(change, max_value)
