5a365e1142c05eae1d0558ec24a126c59e935e69
Wolleaf Lee <744901920@qq.com>
2022-09-17 21:31:21 +0800

    feat:lab06

---
+++ lab/lab06/lab06.py
@@ -0,0 +1,77 @@
+def count_occurrences(t, n, x):
+    """Return the number of times that x appears in the first n elements of iterator t.
+
+    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(s, 10, 9)
+    3
+    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(s2, 3, 10)
+    2
+    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+    >>> count_occurrences(s, 1, 3)
+    1
+    >>> count_occurrences(s, 4, 2)
+    3
+    >>> next(s)
+    2
+    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
+    >>> count_occurrences(s2, 6, 6)
+    2
+    """
+    "*** YOUR CODE HERE ***"
+    times = 0
+    for i in t:
+        n -= 1
+        if i == x:
+            times += 1
+        if n == 0:
+            break
+    return times
+
+
+def scale(it, multiplier):
+    """Yield elements of the iterable it multiplied by a number multiplier.
+
+    >>> m = scale([1, 5, 2], 5)
+    >>> type(m)
+    <class 'generator'>
+    >>> list(m)
+    [5, 25, 10]
+
+    >>> m = scale([1, 2, 3, 4, 5, 6, 7, 8], 2)
+    >>> [next(m) for _ in range(5)]
+    [2, 4, 6, 8, 10]
+    """
+    "*** YOUR CODE HERE ***"
+    for i in it:
+        yield i * multiplier
+
+
+def hailstone(n):
+    """Yields the elements of the hailstone sequence starting at n.
+
+    >>> for num in hailstone(10):
+    ...     print(num)
+    10
+    5
+    16
+    8
+    4
+    2
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    yield n
+    if n == 1:
+        pass
+    elif n % 2 == 0:
+        yield from hailstone(n // 2)
+    else:
+        yield from hailstone(n * 3 + 1)
+    # while n != 1:
+    #     yield n
+    #     if n % 2 == 0:
+    #         n = n // 2
+    #     else:
+    #         n = n * 3 + 1
+    # yield n
